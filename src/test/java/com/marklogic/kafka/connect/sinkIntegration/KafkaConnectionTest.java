a7ca1ca4b67a9f1fbd44e5e39d399aed2d3191df
Phil Barber <Phil.Barber@marklogic.com>
2019-10-26 15:14:46 -0400

    Minimum additions to create and demonstrate an integration test using a Kafka containerfrom testcontainers

--- src/test/java/com/marklogic/kafka/connect/sinkIntegration/KafkaConnectionTest.java
+++ src/test/java/com/marklogic/kafka/connect/sinkIntegration/KafkaConnectionTest.java
@@ -1,4 +1,86 @@
 package com.marklogic.kafka.connect.sinkIntegration;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.Rule;
+import org.junit.jupiter.api.Test;
+import org.rnorth.ducttape.unreliables.Unreliables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.shaded.com.google.common.collect.ImmutableMap;
+
+import java.util.Arrays;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
 public class KafkaConnectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConnectionTest.class);
+
+    @Rule
+    public KafkaContainer kafkaContainer = new KafkaContainer();
+
+    @Test
+    void testKafkaConnectivity() throws Exception {
+        kafkaContainer.start();
+        String kafkaURL = kafkaContainer.getBootstrapServers();
+        logger.info("kafkaURL: " + kafkaURL);
+        testKafkaFunctionality(kafkaURL);
+    }
+
+    protected void testKafkaFunctionality(String bootstrapServers) throws Exception {
+        try (
+                KafkaProducer<String, String> producer = new KafkaProducer<>(
+                        ImmutableMap.of(
+                                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                                ProducerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString()
+                        ),
+                        new StringSerializer(),
+                        new StringSerializer()
+                );
+
+                KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(
+                        ImmutableMap.of(
+                                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+                                ConsumerConfig.GROUP_ID_CONFIG, "tc-" + UUID.randomUUID(),
+                                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"
+                        ),
+                        new StringDeserializer(),
+                        new StringDeserializer()
+                );
+        ) {
+            String topicName = "messages";
+            consumer.subscribe(Arrays.asList(topicName));
+
+            producer.send(new ProducerRecord<>(topicName, "testcontainers", "rulezzz")).get();
+
+            Unreliables.retryUntilTrue(10, TimeUnit.SECONDS, () -> {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+
+                if (records.isEmpty()) {
+                    return false;
+                }
+
+                assertThat(records)
+                        .hasSize(1)
+                        .extracting(ConsumerRecord::topic, ConsumerRecord::key, ConsumerRecord::value)
+                        .containsExactly(tuple(topicName, "testcontainers", "rulezzz"));
+
+                return true;
+            });
+
+            consumer.unsubscribe();
+        }
+    }
 }
