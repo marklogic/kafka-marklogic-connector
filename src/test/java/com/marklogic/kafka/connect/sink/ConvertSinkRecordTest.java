3816a089d82883873e0a48442f8016d9fe3f0e29
Isaac Owusu <ikekykay@gmail.com>
2019-12-02 15:24:57 -0500

    fixed issues with PR

--- src/test/java/com/marklogic/kafka/connect/sink/ConvertSinkRecordTest.java
+++ src/test/java/com/marklogic/kafka/connect/sink/ConvertSinkRecordTest.java
@@ -31,6 +31,8 @@ public class ConvertSinkRecordTest {
 		config.put("ml.document.permissions", "manage-user,read,manage-admin,update");
 		config.put("ml.document.uriPrefix", "/example/");
 		config.put("ml.document.uriSuffix", ".json");
+		config.put("addTopicToCollections", "true");
+		config.put("addTopicToCollections", "true");
 		converter = new DefaultSinkRecordConverter(config);
 
 		DocumentWriteOperation op = converter.convert(newSinkRecord("test"));
