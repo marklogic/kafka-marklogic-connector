f25c7d5bb5634bd75701ee2edb59a069e6bfcaa5
Rob Rudin <rob.rudin@marklogic.com>
2020-03-30 12:49:18 -0400

    Can now run a flow using DHF 5.2.0
    The key changes are in MarkLogicSinkTask and RunFlowWriteBatchListener. See the comments in those classes for the changes.
    
    There are 3 new properties for using this - a DHF flow name, and optional set of steps, and an option for logging each flow response. If the flow name is set, then that flow will be run. Otherwise, only the regular ingestion happens. 
    
    I had to make one plumbing change - DatabaseClientCreator is now DatabaseClientConfigBuilder, as I needed to reuse the DatabaseClientConfig object. That's a small plumbing change though that could be made independently of this change.

---
+++ src/main/java/com/marklogic/kafka/connect/DatabaseClientConfigBuilder.java
@@ -0,0 +1,14 @@
+package com.marklogic.kafka.connect;
+
+import com.marklogic.client.ext.DatabaseClientConfig;
+
+import java.util.Map;
+
+/**
+ * Defines how a map of properties read in by Kafka are used to build an instance of DatabaseClientConfig.
+ */
+public interface DatabaseClientConfigBuilder {
+
+	DatabaseClientConfig buildDatabaseClientConfig(Map<String, String> kafkaConfig);
+
+}
