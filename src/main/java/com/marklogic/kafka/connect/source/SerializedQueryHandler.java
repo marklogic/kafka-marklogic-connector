a93cbe287986236420ca38e900bf005905df0d90
Rita Chen <ritac@progress.com>
2026-09-03 14:48:15 -0400

    MLE-32194 update Copyright

--- src/main/java/com/marklogic/kafka/connect/source/SerializedQueryHandler.java
+++ src/main/java/com/marklogic/kafka/connect/source/SerializedQueryHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2025 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
+ * Copyright (c) 2019-2026 Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.
  */
 package com.marklogic.kafka.connect.source;
 
